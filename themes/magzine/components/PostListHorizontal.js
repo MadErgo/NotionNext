b762ce75ac76f65d60c90ed7c6ab8cd2f2a5d275
MadErgo <madergo66@gmail.com>
2025-07-01 11:15:46 +0800

    Update PostListHorizontal.js

--- themes/magzine/components/PostListHorizontal.js
+++ themes/magzine/components/PostListHorizontal.js
@@ -41,7 +41,7 @@ const PostListHorizontal = ({ title, href, posts, hasBg }) => {
           <Swiper posts={posts} />
           {href && (
             <Link className='lg:hidden block text-lg underline' href={href}>
-              <span>查看全部</span>
+              <span>ALL</span>
               <i className='ml-2 fas fa-arrow-right' />
             </Link>
           )}
